f037f283bfa8ba2d260accdf67c3ac555b44ef73
miguel <migueldeaguiarpaula07@gmail.com>
2025-10-30 22:15:55 -0300

    finalizando ultimo commit da aula

--- git.md
+++ git.md
@@ -16,3 +16,12 @@ em java script precisa usar " quando é um texto
 
 texto = string
 juntar texto = concatenar
+
+em nomes de pastas ou arquivos, não usar numeros nem espaços 
+
+
+// criando variavel
+// declarando variavel e atribuindo 102
+const mediaNotas // CamelCase
+const mediaNotas // PasquelCase
+const media_notas // Snacke_Case
--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,17 @@
 console.log("fusca azul")
+
+// criando variavel
+// declarando variavel e atribuindo 102
+const mediaNotas // CamelCase
+const mediaNotas // PasquelCase
+const media_notas // Snacke_Case
+
+
+let numero = 102
+console.log(numero)
+numero = 99
+console.log(numero)
+
+const nome = "miguel" 
+console.log(nome)
+//nome = "batata" - não pode atribuir constante depois de declarar
